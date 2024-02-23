2b5248e6048c08235f45d183ac42443d664bd850
wtto00 <wtto00@outlook.com>
2024-02-24 00:37:21 +0800

    更名为uniapp，uni与官方的重名

--- README.md
+++ README.md
@@ -2,44 +2,6 @@
 
 CLI for Uniapp
 
-## TODO
+## Feat
 
-- [ ] test requirments
-- [ ] platform
-- [ ] run
-- [ ] build
-
-## Help
-
-```shell
-uniapp
-# uniapp --help
-# uniapp -h
-```
-
-```text
-uniapp-cli@1.0.0
-
-Synopsis
-
-    uniapp command [options]
-
-Commands
-    create ............................. Create a project
-
-Options
-    -h, --help ......................... Get help for a command
-    -v, --version ...................... prints out this utility's version
-    -d, --verbose ...................... debug mode produces verbose log output for all activity
-```
-
-## Create
-
-```shell
-uniapp create
-```
-
-```text
-App name: Uniapp
-Package name: uniapp-project
-```
+https://github.com/tj/commander.js/issues/385
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,10 +1,9 @@
 import { program } from "commander";
 import { version } from "../package.json";
-import chalk from "chalk";
 import Log from "./utils/log";
 
 program
-  .name("uni")
+  .name("uniapp")
   .version(`uniapp-cli v${version}`)
   .usage("<command> [options]")
   .option("-d, --verbose", "debug mode produces verbose log output for all activity")
@@ -24,7 +23,7 @@ program
       "If all requirements for each platform are met, exits with code 0 otherwise exits with non-zero code."
   )
   .argument("<platform...>", "Platforms requirements you want to check")
-  .addHelpText("after", "\nExample:\n  uni requirements android")
+  .addHelpText("after", "\nExample:\n  uniapp requirements android")
   .action((platforms) => {
     void import("./requirements").then(({ requirements }) => requirements(platforms));
   });
@@ -37,7 +36,7 @@ program
   .argument("<app-name>", "Human readable name")
   .option("-t, --template <template>", "use a custom template from GitHub/GitLab/Bitbucket/Git:url")
   .option("-f, --force", "Overwrite target directory if it exists")
-  .addHelpText("after", "\nExample:\n  uni create MyUniApp")
+  .addHelpText("after", "\nExample:\n  uniapp create MyUniApp")
   .action((appName, options) => {
     void import("./create").then(({ create }) => create(appName, options));
   });
@@ -94,7 +93,7 @@ program
   .option("--target <target>", "Deploy to a specific target\nOnly available on Android and iOS")
   .addHelpText(
     "after",
-    "\nExample:\n  uni run android --release --target=myEmulator\n  uni run ios --device --debug\n  uni run mp-weixin"
+    "\nExample:\n  uniapp run android --release --target=myEmulator\n  uniapp run ios --device --debug\n  uniapp run mp-weixin"
   )
   .action((platform, options) => {
     void import("./run").then(({ run }) => run(platform, options));
